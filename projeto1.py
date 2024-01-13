edf5aa108e451342a9f0036b3da427289bbf1681
Danny Almeida <danyelaalmeida1811@gmail.com>
2024-01-13 16:59:26 -0300

    Projeto 1
    Jogo da velha versão 1 em Python, realizado durante o curso Fundamentos de Linguagem Python para Análise de Dados e Data Science.

---
+++ projeto1.py
@@ -0,0 +1,65 @@
+# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão 1
+
+import random
+from os import system, name
+
+# Função para limar a tela a cada execução
+def limpa_tela():
+ 
+    # Windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # Mac ou Linux
+    else:
+        _ = system('clear')
+
+def game():
+
+    limpa_tela()
+    print("\nBem-vindo(a) ao jogo da forca!")
+    print("Adivinhe a palavra abaixo:\n")
+
+    # Lista de palavras para o jogo
+    palavras = ['banana', 'abacate', 'uva', 'morango', 'laranja']
+    
+    # Escolhe randomicamente uma palavra
+    palavra = random.choice(palavras)
+
+    # List comprehension
+    letras_descobertas = ['_' for letra in palavra]
+    chances = 6
+    letras_erradas = []
+
+    # Loop enquanto número de chances for maior do que zero
+    while chances > 0:
+        print(" ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:", " ".join(letras_erradas))
+
+        # Tentativa
+        tentativa = input("\nDigite uma letra: ").lower()
+
+        if tentativa in palavra:
+            index = 0
+            for letra in palavra:
+                if tentativa == letra:
+                    letras_descobertas[index] = letra
+                index += 1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
+
+        if "_" not in letras_descobertas:
+            print("\nVocê venceu, a palavra era:", palavra)
+            break
+
+    if "_" in letras_descobertas:
+        print("\nVocê perdeu, a palavra era:", palavra)
+
+# Bloco main
+if __name__ == "__main__":
+    game()
+    print("\nObrigada por participar!:)\n")
+
+    
